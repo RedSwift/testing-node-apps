7b4be423945cff7080e0730540544bf4f7dcd325
Dominic Lam <dominic.lam@merck.com>
2021-05-28 16:41:31 +0800

    test: refactor - ObjectFactory method for res

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -3,13 +3,20 @@
 import {UnauthorizedError} from 'express-jwt'
 import errorMiddleware from '../error-middleware'
 
+function buildRes(overrides) {
+  const res = {
+    json: jest.fn(() => res),
+    status: jest.fn(() => res),
+    ...overrides
+  }
+  return res
+}
+
 test('responds with 401 for express-jwt UnauthorizedError', () => {
   const req = {}
   const next = jest.fn()
-  const code = 'some_error_code'
-  const message = 'Some message'
-  const error = new UnauthorizedError(code, {message})
-  const res = {json: jest.fn(() => res), status: jest.fn(() => res)} 
+  const error = new UnauthorizedError('some_error_code', {message: 'Some message'})
+  const res = buildRes()
 
   errorMiddleware(error, req, res, next)
 
@@ -26,7 +33,7 @@ test('skip throwing an error if res has been sent', () => {
   const req = {}
   const error = new Error('blah')
   const next = jest.fn()
-  const res = {json: jest.fn(() => res), status: jest.fn(() => res), headersSent: true}
+  const res = buildRes({headersSent: true})
 
   errorMiddleware(error, req, res, next)
 
@@ -41,7 +48,7 @@ test('returns 500 with error log if unknown error', () => {
   const req = {}
   const error = new Error('blah')
   const next = jest.fn()
-  const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
+  const res = buildRes()
 
   errorMiddleware(error, req, res, next)
 
