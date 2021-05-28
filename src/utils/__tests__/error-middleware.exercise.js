5d94f809f60eed0a5760407e4ae0afadca371f81
Dominic Lam <dominic.lam@merck.com>
2021-05-28 16:09:15 +0800

    test for 401 response

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -1,15 +1,26 @@
 // Testing Middleware
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
+import {UnauthorizedError} from 'express-jwt'
+import errorMiddleware from '../error-middleware'
 
-// 🐨 you'll need both of these:
-// import {UnauthorizedError} from 'express-jwt'
-// import errorMiddleware from '../error-middleware'
+test('responds with 401 for express-jwt UnauthorizedError', () => {
+  const req = {}
+  const next = jest.fn()
+  const code = 'some_error_code'
+  const message = 'Some message'
+  const error = new UnauthorizedError(code, {message})
+  const res = {json: jest.fn(() => res), status: jest.fn(() => res)} 
 
-// 🐨 Write a test for the UnauthorizedError case
-// 💰 const error = new UnauthorizedError('some_error_code', {message: 'Some message'})
-// 💰 const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
+  errorMiddleware(error, req, res, next)
+
+  expect(next).not.toHaveBeenCalled()
+
+  expect(res.status).toHaveBeenCalledTimes(1)
+  expect(res.status).toHaveBeenCalledWith(401)
+
+  expect(res.json).toHaveBeenCalledTimes(1)
+  expect(res.json).toHaveBeenCalledWith({code: error.code, message: error.message})
+});
 
 // 🐨 Write a test for the headersSent case
 
